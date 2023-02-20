3d124571a5fc40da64073486a1babd8981f2800e
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-02-20 16:10:35 +0600

    file speration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ import { useSpeechRecognition } from "react-speech-kit";
 import { default as Speak } from "react-text-to-speech";
 import { BsMic } from "react-icons/bs";
 import axios from "axios";
-
+import GenerateImage from "./GenerateImage";
 import { BsMicFill } from "react-icons/bs";
 import { BsMicMuteFill } from "react-icons/bs";
 
@@ -76,31 +76,31 @@ function App() {
   // const [text, setText] = useState('');
   const [imageURL, setImageURL] = useState("");
 
-  const generateImage = async () => {
-    if (inputPrompt.startsWith("draw")) {
-      try {
-        const response = await axios({
-          method: "post",
-          url: "https://api.openai.com/v1/images/generations",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${API_KEY}`,
-          },
-          data: {
-            model: "image-alpha-001",
-            prompt: inputPrompt,
-            num_images: 1,
-            size: "512x512",
-            response_format: "url",
-          },
-        });
-        setImageURL(response.data.data[0].url);
-        setIsImageGenerated(true);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  };
+  // const generateImage = async () => {
+  //   if (inputPrompt.startsWith("draw")) {
+  //     try {
+  //       const response = await axios({
+  //         method: "post",
+  //         url: "https://api.openai.com/v1/images/generations",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //           Authorization: `Bearer ${API_KEY}`,
+  //         },
+  //         data: {
+  //           model: "image-alpha-001",
+  //           prompt: inputPrompt,
+  //           num_images: 1,
+  //           size: "512x512",
+  //           response_format: "url",
+  //         },
+  //       });
+  //       setImageURL(response.data.data[0].url);
+  //       setIsImageGenerated(true);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
+  // };
 
   return (
     <div className="App">
@@ -304,10 +304,12 @@ function App() {
                       {chat.botMessage ? (
                         <div id="botMessage" ref={messagesEndRef}>
                         
-                          <img src={imageURL}  />
+                          {/* <img src={imageURL}  /> */}
+
+                          
                           <BotResponse response={chat.botMessage} />
 
-
+                        <GenerateImage/>
                           <Speak
                             text={chat.botMessage}
                             startBtn={
@@ -386,7 +388,7 @@ function App() {
               <BsMic />
             </button>
             <button
-              onClick={generateImage}
+              // onClick={generateImage}
               className="send-button"
               type="submit"
               style={{
