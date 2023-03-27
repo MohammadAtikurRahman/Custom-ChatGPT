5bccb177e8b6c49beb9e6759e6b7066b08800f55
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-03-27 15:58:27 +0600

    idiya chatgpt is ready

--- frontend/src/components/IntroSection.jsx
+++ frontend/src/components/IntroSection.jsx
@@ -2,15 +2,15 @@ import React from "react";
 const IntroSection = () => {
   return (
     <div id="introsection">
-      <h1>Introducing Custom ChatGPT</h1>
+      <h1>Introducing Idiya ChatGPT</h1>
       <p>
         Features:
         <ul>
-          <li>Instantaneous responses to all queries</li>
-          <li>Trained to decline inappropriate requests </li>
-          <li>Continuously Learning</li>
-          <li>Improved efficiency and accuracy in NLP-related tasks</li>
-          <li>pleasant user interface</li>
+          <li>Product name + price,sku,brand,weight,height,dimension</li>
+          <li>Product name+ price+ shipping name</li>
+          <li>Product name + shipping name</li>
+          <li>recom + Product name = Recommended Product(max 10)</li>
+          <li>sku,Product name = description</li>
         </ul>
       </p>
     </div>
