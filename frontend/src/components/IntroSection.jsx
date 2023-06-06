c36852d20317287a348d50916840fe6d55bc3394
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-06-06 16:01:40 +0600

    present added button

--- frontend/src/components/IntroSection.jsx
+++ frontend/src/components/IntroSection.jsx
@@ -1,20 +1,30 @@
-import React from "react";
-const IntroSection = () => {
+import React, { useState } from 'react';
+
+function YourComponent() {
+  const [message, setMessage] = useState('');
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleClick = (message) => {
+    setMessage(message);
+    setShowMessage(true);
+  };
+
   return (
     <div id="introsection">
       <h1>Introducing Idiya ChatGPT</h1>
       <p>
         Features:
         <ul>
-          <li>Product name + price,sku,brand,weight,height,dimension</li>
-          <li>Product name+ price+ shipping name</li>
-          <li>Product name + shipping name</li>
-          <li>recom + Product name = Recommended Product(max 10)</li>
-          <li>sku,Product name = description</li>
+          <li><button onClick={() => handleClick('Message 1')}>Product name + price, sku, brand, weight, height, dimension</button></li>
+          <li><button onClick={() => handleClick('Message 2')}>Product name + price + shipping name</button></li>
+          <li><button onClick={() => handleClick('Message 3')}>Product name + shipping name</button></li>
+          <li><button onClick={() => handleClick('Message 4')}>recom + Product name = Recommended Product (max 10)</button></li>
+          <li><button onClick={() => handleClick('Message 5')}>sku, Product name = description</button></li>
         </ul>
       </p>
+      {showMessage && <div style={{ position: "fixed" , bottom: "6.3%",left: "25%", zIndex: "9999"}} id="messageBox">{message}</div>}
     </div>
   );
-};
+}
 
-export default IntroSection;
+export default YourComponent;
